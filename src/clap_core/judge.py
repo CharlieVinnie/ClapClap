a7bbaa763e51d0dbb3f52305e509c9a68de8ce66
Charlie <charliewang.cpp@gmail.com>
2025-02-28 18:02:32 +0800

    judge.py test passed

--- src/clap_core/judge.py
+++ src/clap_core/judge.py
@@ -1,5 +1,12 @@
 from enum import Enum
 from clap_core.player import Player
+from clap_core.hands import kills
+
+class ClapInternalError(Exception):
+    pass
+
+class GamePlayError(Exception):
+    pass
 
 class GameStatus(Enum):
     ONGOING = 0
@@ -10,9 +17,24 @@ class Judge:
     
     def __init__(self) -> None:
         self.player = ( Player(), Player() )
+        self.status = GameStatus.ONGOING
 
     def play(self, hand1: str, hand2: str):
-        pass
+        if self.status != GameStatus.ONGOING:
+            raise GamePlayError("Game has already ended")
+        
+        self.player[0].play(hand1)
+        self.player[1].play(hand2)
+
+        first_win = kills(hand1,hand2)
+        second_win = kills(hand2,hand1)
+
+        if first_win and second_win:
+            raise ClapInternalError("Mutual killing")
+        elif first_win:
+            self.status = GameStatus.FIRSTWIN
+        elif second_win:
+            self.status = GameStatus.SECONDWIN
 
     def gameStatus(self):
-        return GameStatus.ONGOING
+        return self.status
