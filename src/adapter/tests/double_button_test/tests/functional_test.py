58c67eb7f3bc30741ea9a83f19dfd598bd7c601c
Charlie <charliewang.cpp@gmail.com>
2025-03-07 23:31:36 +0800

    remove logging test

--- src/adapter/tests/double_button_test/tests/functional_test.py
+++ src/adapter/tests/double_button_test/tests/functional_test.py
@@ -1,6 +1,4 @@
 import threading
-import logging
-# import pygame
 import pytest
 from ..controller.main_control import gameEntrance
 from ....testing.fixture import pygame_gui_testing # type: ignore
@@ -8,14 +6,3 @@ from ....testing.fixture import pygame_gui_testing # type: ignore
 @pytest.mark.parametrize("pygame_gui_testing", [gameEntrance], indirect=True)
 def test_double_button(caplog: pytest.LogCaptureFixture, pygame_gui_testing: threading.Thread):
     pass
-
-
-
-
-def test_logging(caplog: pytest.LogCaptureFixture):
-    logger = logging.getLogger()
-
-    with caplog.at_level(logging.INFO):
-        logger.info("hello")
-
-    assert "hello" in caplog.text
