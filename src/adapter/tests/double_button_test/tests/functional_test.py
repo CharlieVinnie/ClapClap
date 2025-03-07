0e16013fcdee2ebb9657daee0bffdb653a538656
Charlie <charliewang.cpp@gmail.com>
2025-03-07 23:01:05 +0800

    use fixture to automate

--- src/adapter/tests/double_button_test/tests/functional_test.py
+++ src/adapter/tests/double_button_test/tests/functional_test.py
@@ -1,8 +1,18 @@
 import threading
-# import pygame
+import pygame
 import pytest
 from adapter.tests.double_button_test.controller.main_control import gameEntrance
 
-def test_double_button(caplog: pytest.LogCaptureFixture):
+@pytest.fixture
+def pygame_gui_testing():
     main_thread = threading.Thread(target=gameEntrance)
     main_thread.start()
+
+    yield main_thread
+
+    pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+
+
+def test_double_button(caplog: pytest.LogCaptureFixture, pygame_gui_testing: threading.Thread):
+    pass
