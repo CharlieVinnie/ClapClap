c694abee2d4c2da3ebb4710d7da997e977152640
Charlie <charliewang.cpp@gmail.com>
2025-03-08 16:36:53 +0800

    add testadp.sh

---
+++ testadp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PYTHONPATH="/home/charlie/ClapClap/src"
+
+# python3 /home/charlie/ClapClap/src/adapter/tests/double_button_test/controller/main_control.py
+python3 -m src.adapter.tests.double_button_test.controller.main_control
