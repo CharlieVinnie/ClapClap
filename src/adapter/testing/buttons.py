02a8851351178d531746d5e710437de0cbfbf9b3
Charlie <charliewang.cpp@gmail.com>
2025-03-10 12:34:03 +0800

    bad pyautogui

--- src/adapter/testing/buttons.py
+++ src/adapter/testing/buttons.py
@@ -1,4 +1,9 @@
 from pygame_gui.elements import UIButton
+import pyautogui
+from pygame import Rect
+import typing
 
 def simulate_click_button(button: UIButton):
-    pass
+    rect = typing.cast(Rect, button.rect) # type: ignore
+    x,y = rect.center
+    pyautogui.click(x,y) # type: ignore
