0a22b43226c2ae0215d0b30199565e6d3ebb62da
Charlie <charliewang.cpp@gmail.com>
2025-03-13 16:27:47 +0800

    remove other button from "single button test"

--- tests/adapter_tests/double_button_test/double_button_functional_test.py
+++ tests/adapter_tests/double_button_test/double_button_functional_test.py
@@ -3,14 +3,7 @@ import threading
 import pytest
 from .controller.main_control import gameEntrance
 from adapter import pygame_gui_testing # type: ignore
-# from ....testing.buttons import simulate_click_button
-# import typing
-# from pygame_gui.elements import UIButton
 
 @pytest.mark.parametrize("pygame_gui_testing", [gameEntrance], indirect=True)
 def test_double_button(caplog: pytest.LogCaptureFixture, pygame_gui_testing: threading.Thread):
     adapter.findElement("#start_dummy_button")
-    # assert findElement("#start_dummy_button") is not None
-    # dummy = typing.cast(UIButton, findElement("#start_dummy_button"))
-    # simulate_click_button(dummy)
-    # assert findElement("#start_dummy_button") is not None
--- tests/adapter_tests/single_button_test/views/buttons.py
+++ tests/adapter_tests/single_button_test/views/buttons.py
@@ -1,8 +1,6 @@
 import adapter
-import typing
 from typing import Callable
 import pygame
-from pygame_gui.elements import UIButton
 
 class ButtonNotFoundError(Exception): pass
 
@@ -13,18 +11,3 @@ def createStartDummyButton(callback: Callable[[],None]):
                                   { "object_id": "#start_dummy_button",
                                     "text": "Dummy",
                                     "relative_rect": pygame.Rect((100,100),(200,50)), } )
-
-
-def createOtherButton(callback: Callable[[],None]):
-
-    adapter.create_button(callback,
-                                  config=
-                                  { "object_id": "#other_button",
-                                    "text": "Hello!",
-                                    "relative_rect": pygame.Rect((500,500),(200,50)), } )
-
-def removeOtherButton():
-    
-    button = typing.cast(UIButton, adapter.findElement("#other_button"))
-    adapter.remove_button(button)
-    button.kill()
