12ed89c1de4caeb9ff1fb36c9d834a126e226577
Charlie <charliewang.cpp@gmail.com>
2025-03-13 16:36:11 +0800

    finds other button

--- tests/adapter_tests/double_button_test/double_button_functional_test.py
+++ tests/adapter_tests/double_button_test/double_button_functional_test.py
@@ -6,4 +6,11 @@ from adapter import pygame_gui_testing # type: ignore
 
 @pytest.mark.parametrize("pygame_gui_testing", [gameEntrance], indirect=True)
 def test_double_button(caplog: pytest.LogCaptureFixture, pygame_gui_testing: threading.Thread):
-    adapter.findElement("#start_dummy_button")
+
+    dummy_button = adapter.findButton("#start_dummy_button")
+
+    adapter.simulate_click_button(dummy_button)
+
+    adapter.findButton("#other_button")
+
+    
